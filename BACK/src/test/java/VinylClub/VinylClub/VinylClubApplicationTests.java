25fc639d0f597031c97a05c2084818810aca409b
Sebus83 <sebastien.herbiet@gmail.com>
2025-04-28 11:50:23 +0200

    creation dossier BACK

---
+++ BACK/src/test/java/VinylClub/VinylClub/VinylClubApplicationTests.java
@@ -0,0 +1,13 @@
+package VinylClub.VinylClub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinylClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
