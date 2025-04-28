679fc9af55f6422e02aa37b7d1b765e2e3e8bef9
Sebus83 <sebastien.herbiet@gmail.com>
2025-04-28 11:50:23 +0200

    creation dossier BACK

---
+++ BACK/src/main/java/VinylClub/VinylClub/VinylClubApplication.java
@@ -0,0 +1,13 @@
+package VinylClub.VinylClub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinylClubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VinylClubApplication.class, args);
+	}
+
+}
