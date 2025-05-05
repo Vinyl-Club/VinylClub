1d77c85597d8e6a42285350591dcd4835d5856a6
Sebus83 <sebastien.herbiet@gmail.com>
2025-05-05 16:08:45 +0200

    ajout gettere setter album

--- BACK/vinyl-catalog-service/src/main/java/com/vinylclub/catalog/entity/Product.java
+++ BACK/vinyl-catalog-service/src/main/java/com/vinylclub/catalog/entity/Product.java
@@ -3,6 +3,7 @@ package com.vinylclub.catalog.entity;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -13,6 +14,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
 
 @Entity
 @Table(name = "products")
@@ -158,6 +160,14 @@ public class Product {
 
     public void setArtist(Artist artist) {
         this.artist = artist;
+    }
+
+    public Category getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(Category album) {
+        this.album = album;
     }
 
     public Integer getQuantity() {
