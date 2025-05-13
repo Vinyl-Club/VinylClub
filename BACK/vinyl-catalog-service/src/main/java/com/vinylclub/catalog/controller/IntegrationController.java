0b165f8856f78f705cf1ab600d5392b6fd75ad65
Sebus83 <sebastien.herbiet@gmail.com>
2025-05-13 15:32:02 +0200

    docker

---
+++ BACK/vinyl-catalog-service/src/main/java/com/vinylclub/catalog/controller/IntegrationController.java
@@ -0,0 +1,24 @@
+package com.vinylclub.catalog.controller;
+
+import com.vinylclub.catalog.dto.UserDTO;
+import com.vinylclub.catalog.service.UserIntegrationService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/integration")
+public class IntegrationController {
+    
+    private final UserIntegrationService userIntegrationService;
+    
+    public IntegrationController(UserIntegrationService userIntegrationService) {
+        this.userIntegrationService = userIntegrationService;
+    }
+    
+    @GetMapping("/users/{id}")
+    public UserDTO getUserDetails(@PathVariable Long id) {
+        return userIntegrationService.getUserDetails(id);
+    }
+}
