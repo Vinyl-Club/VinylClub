d11804d54578fdf054a18d8a27c442e7d69ad904
CharlotteBCK <charlotte44300@gmail.com>
2025-10-24 16:27:05 +0200

    premier test pour la connexion back et front

---
+++ FRONT-WEB/vinyl-club/src/app/(public)/page.tsx
@@ -0,0 +1,9 @@
+// FRONT-WEB/vinyl-club/src/app/(public)/page.tsx
+export default function HomePage() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>Bienvenue sur Vinyl Club</h1>
+      <p>Page d’accueil OK ✅</p>
+    </main>
+  );
+}
--- FRONT-WEB/vinyl-club/src/app/layout.tsx
+++ FRONT-WEB/vinyl-club/src/app/layout.tsx
@@ -1,7 +1,14 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Vinyl Club',
+  description: 'Marketplace de vinyles',
+};
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-    return (
-        <html lang="fr">
-        <body>{children}</body>
-        </html>
-    );
+  return (
+    <html lang="fr">
+      <body>{children}</body>
+    </html>
+  );
 }
