884b01b65d2b24a6655a07e33c6d2f0eefd74a38
flo <boireaufloriane@hotmail.fr>
2025-11-20 17:41:20 +0100

    mise enn place du feightclinet

--- BACK/vinyl-ad-service/src/main/java/com/vinylclub/ad/client/UserClient.java
+++ BACK/vinyl-ad-service/src/main/java/com/vinylclub/ad/client/UserClient.java
@@ -1,3 +1,10 @@
-public class UserClient {
-    
+package com.vinylclub.ad.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import com.vinylclub.ad.client.dto.UserSummaryDTO;
+public interface UserClient {
+    @GetMapping("/api/users/{id}")
+    UserDTO getUserById(@PathVariable("id") Long id);
 }
