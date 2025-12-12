6f7a73d6138d7c3c923c7002690a775d21d91b39
CharlotteBCK <charlotte44300@gmail.com>
2025-12-12 16:42:35 +0100

    en cours

---
+++ BACK/vinyl-ad-service/src/main/java/com/vinylclub/ad/client/dto/ProductCreatedDTO.java
@@ -0,0 +1,9 @@
+package com.vinylclub.ad.client.dto;
+
+public class ProductCreatedDTO {
+    private Long id;
+    public ProductCreatedDTO() {}
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+}
+
--- BACK/vinyl-ad-service/src/main/java/com/vinylclub/ad/client/request/CreateProductRequestDTO.java
+++ BACK/vinyl-ad-service/src/main/java/com/vinylclub/ad/client/request/CreateProductRequestDTO.java
@@ -1,3 +1,68 @@
+package com.vinylclub.ad.client.dto;
+
+import java.math.BigDecimal;
+
 public class CreateProductRequestDTO {
-    
+    private String title;
+    private String description;
+    private BigDecimal price;
+    private String status;
+    private String state;
+    private String format;
+
+    private IdDTO artist;
+    private IdDTO category;
+    private IdDTO album;
+
+    public CreateProductRequestDTO() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+       public String getFormat() {
+        return format;
+    }
+
+    public void setFormat(String format) {
+        this.format = format;
+    }
+
 }
