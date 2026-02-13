7a3bc51f8bd64589df803f2d0269e965e0526770
Flo <boireaufloriane@hotmail.fr>
2026-02-13 09:19:16 +0100

    ouverture de la PR

---
+++ BACK/vinyl-catalog-service/src/test/java/com/vinylClub/catalog/servive/FilterProductTest.java
@@ -0,0 +1,64 @@
+package com.vinylclub.catalog.service;
+
+import com.vinylclub.catalog.dto.ProductDTO;
+import com.vinylclub.catalog.dto.AlbumDTO;
+import com.vinylclub.catalog.dto.ArtistDTO;
+import com.vinylclub.catalog.dto.CategoryDTO;
+import com.vinylclub.catalog.dto.ImageDTO;
+import com.vinylclub.catalog.dto.ImageSummaryDTO;
+
+import com.vinylclub.catalog.entity.Product;
+import com.vinylclub.catalog.entity.Album;
+import com.vinylclub.catalog.entity.Artist;
+import com.vinylclub.catalog.entity.Category;
+import com.vinylclub.catalog.entity.ProductState;
+import com.vinylclub.catalog.entity.ProductStatus;
+import com.vinylclub.catalog.entity.ProductFormat;
+
+import com.vinylclub.catalog.repository.ProductRepository;
+import com.vinylclub.catalog.repository.AlbumRepository;
+import com.vinylclub.catalog.repository.ArtistRepository;
+import com.vinylclub.catalog.repository.CategoryRepository;
+import com.vinylclub.catalog.repository.ImageRepository;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.PageImpl;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+import org.springframework.data.domain.PageRequest;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class FilterProductTest {
+
+    @Mock
+    private ProductRepository productRepository;
+    @Mock
+    private AlbumRepository albumRepository;
+    @Mock
+    private ArtistRepository artistRepository;
+    @Mock
+    private CategoryRepository categoryRepository;
+
+    @InjectMocks
+    private ProductService productService;
+
+    @Test
+    public void searchProducts_shouldReturnProductDto_whenProductfindByArtistOrAlbum() {
+
+        
+    }
+}
