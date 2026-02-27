6d452e4acc07498c8fe5e6bd3dc8e2d32dfc86a6
CharlotteBCK <charlotte44300@gmail.com>
2026-02-27 10:19:17 +0100

    home page en cours

--- FRONT-WEB/vinyl-club/src/app/(public)/page.tsx
+++ FRONT-WEB/vinyl-club/src/app/(public)/page.tsx
@@ -1,6 +1,16 @@
-import { redirect } from 'next/navigation';
+import { catalog } from '@/features/catalog/api';
+import CatalogView from '@/features/catalog/view/CatalogView';
 
-export default function Page() {
-  redirect('/catalog');
+export default async function Page() {
+  const items = await catalog();
+
+  return (
+    <>
+      <h1 style={{ fontSize: '40px', marginBottom: '30px' }}>
+        BONJOUR TEST
+      </h1>
+
+      <CatalogView items={items} />
+    </>
+  );
 }
-
