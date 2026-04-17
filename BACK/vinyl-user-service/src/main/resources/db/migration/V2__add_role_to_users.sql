ef326eb81ca3ae4fa19edbb71b89d0feced11df0
Flo <boireaufloriane@hotmail.fr>
2026-04-17 09:50:00 +0200

    modif migration pour récréer la bdd

--- BACK/vinyl-user-service/src/main/resources/db/migration/V2__add_role_to_users.sql
+++ BACK/vinyl-user-service/src/main/resources/db/migration/V2__add_role_to_users.sql
@@ -1,29 +1,29 @@
--- 1) ajoute la colonne si elle n'existe pas
-ALTER TABLE users.users
-ADD COLUMN IF NOT EXISTS role VARCHAR(20);
+-- -- 1) ajoute la colonne si elle n'existe pas
+-- ALTER TABLE users.users
+-- ADD COLUMN IF NOT EXISTS role VARCHAR(20);
 
--- 2) met USER sur toutes les lignes existantes
-UPDATE users.users
-SET role = 'USER'
-WHERE role IS NULL;
+-- -- 2) met USER sur toutes les lignes existantes
+-- UPDATE users.users
+-- SET role = 'USER'
+-- WHERE role IS NULL;
 
--- 3) défaut USER pour les futurs users (même si le code le fait déjà)
-ALTER TABLE users.users
-ALTER COLUMN role SET DEFAULT 'USER';
+-- -- 3) défaut USER pour les futurs users (même si le code le fait déjà)
+-- ALTER TABLE users.users
+-- ALTER COLUMN role SET DEFAULT 'USER';
 
-DO $$
-BEGIN
-  IF NOT EXISTS (
-    SELECT 1
-    FROM pg_constraint c
-    JOIN pg_class t ON t.oid = c.conrelid
-    JOIN pg_namespace n ON n.oid = t.relnamespace
-    WHERE c.conname = 'users_role_check'
-      AND t.relname = 'users'
-      AND n.nspname = 'users'
-  ) THEN
-    ALTER TABLE users.users
-      ADD CONSTRAINT users_role_check
-      CHECK (role IN ('USER', 'ADMIN'));
-  END IF;
-END $$;
+-- DO $$
+-- BEGIN
+--   IF NOT EXISTS (
+--     SELECT 1
+--     FROM pg_constraint c
+--     JOIN pg_class t ON t.oid = c.conrelid
+--     JOIN pg_namespace n ON n.oid = t.relnamespace
+--     WHERE c.conname = 'users_role_check'
+--       AND t.relname = 'users'
+--       AND n.nspname = 'users'
+--   ) THEN
+--     ALTER TABLE users.users
+--       ADD CONSTRAINT users_role_check
+--       CHECK (role IN ('USER', 'ADMIN'));
+--   END IF;
+-- END $$;
